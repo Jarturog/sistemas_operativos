d5accc922595a6c72284b795a64885ffc05149a9
Jarturog <69714460+Jarturog@users.noreply.github.com>
2022-10-28 22:20:26 +0200

    Update strcpyANDstrncpy.c

--- Ficheros fuente para los Retos 1 y 2 de la Práctica 1-20221005/strcpyANDstrncpy.c
+++ Ficheros fuente para los Retos 1 y 2 de la Práctica 1-20221005/strcpyANDstrncpy.c
@@ -10,8 +10,8 @@ char *my_strcpy(char *dest, const char *src){
 
 char *my_strncpy(char *dest, const char *src, size_t n){
   int i = 0;
-  if(strlen(dest) < n){
-    n = strlen(dest)+1;
+  if(my_strlen(dest) < n){
+    n = my_strlen(dest)+1;
   }
   while(src[i] && n > 0){
     dest[i] = src[i];
