52ded2a549a9690739ed5ca59773e0523329bca8
Notbadtrips <105052342+Notbadtrips@users.noreply.github.com>
2022-12-01 12:39:04 +0100

    Update nivel3.c

--- practica2/nivel3.c
+++ practica2/nivel3.c
@@ -41,6 +41,17 @@ void jobs_list_update(int idx, pid_t pid, char status, char cmd[]){
 
 
 // declaraciones de funciones
+char *read_line(char *line);
+void imprimir_prompt();
+int parse_args(char **args, char *line);
+int execute_line(char *line);
+int check_internal(char **args);
+int internal_cd(char **args);
+int internal_export(char **args);
+int internal_source(char **args);
+int internal_jobs(char **args);
+int internal_fg(char **args);
+int internal_bg(char **args);
 
 int main(char *argv[])
 {
