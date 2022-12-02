db7343ac453f837a8b5ef28df97b04e95b344d55
Jarturog <69714460+Jarturog@users.noreply.github.com>
2022-12-02 15:52:46 +0100

    Update nivel1.c

--- practica2/nivel1.c
+++ practica2/nivel1.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <colores.h>
+#include "colores.h"
 #define _POSIX_C_SOURCE 200112L
 #define COMMAND_LINE_SIZE 1024
 #define ARGS_SIZE 64
 #define DEBUGN1 0
 #define PROMPT '$'
 // declaraciones de funciones
+char *read_line(char *line);
+void imprimir_prompt();
+int parse_args(char **args, char *line);
+int execute_line(char *line);
+int check_internal(char **args);
+int internal_cd(char **args);
+int internal_export(char **args);
+int internal_source(char **args);
+int internal_jobs(char **args);
+int internal_fg(char **args);
+int internal_bg(char **args);
 
 int main()
 {
@@ -25,8 +36,7 @@ int main()
 
 char *read_line(char *line)
 {
-    printf(imprimir_prompt());
-    fflush(stdout);
+    imprimir_prompt();
     if (fgets(line, COMMAND_LINE_SIZE, stdin))
     {
         line[COMMAND_LINE_SIZE - 1] = '\0'; // substituyo el carácter final por \0
@@ -39,11 +49,12 @@ char *read_line(char *line)
     }
     return NULL;
 }
-char *imprimir_prompt()
+void imprimir_prompt()
 {
     char cwd[COMMAND_LINE_SIZE];
     getcwd(cwd, COMMAND_LINE_SIZE);
-    return ("%s %s %c ", getenv("USER"), cwd, PROMPT);
+    printf("%s %s %c ", getenv("USER"), cwd, PROMPT);
+    fflush(stdout);
 }
 int execute_line(char *line)
 {
