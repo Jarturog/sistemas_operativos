872b4895747e80e95b523f5960f34ea8ddfbb581
Jarturog <69714460+Jarturog@users.noreply.github.com>
2022-12-02 17:59:22 +0100

    arreglado

--- practica2/nivel2.c
+++ practica2/nivel2.c
@@ -3,23 +3,34 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <string.h>
-#include <colores.h>
-#define _POSIX_C_SOURCE 200112L
+#include "colores.h"
+//#define _POSIX_C_SOURCE 200112L
 #define COMMAND_LINE_SIZE 1024
 #define ARGS_SIZE 64
-#define DEBUGN1 -1
-#define DEBUGN2 0
+#define DEBUGN1 0
+#define DEBUGN2 -1
 #define PROMPT '$'
 #define SUCCESS 0
 #define FAILURE -1
 // declaraciones de funciones
+char *read_line(char *line);
+void imprimir_prompt();
+int parse_args(char **args, char *line);
+int execute_line(char *line);
+int check_internal(char **args);
+int internal_cd(char **args);
+int internal_export(char **args);
+int internal_source(char **args);
+int internal_jobs(char **args);
+int internal_fg(char **args);
+int internal_bg(char **args);
 
 int main()
 {
     char line[COMMAND_LINE_SIZE];
     while (1)
     {
-        if (read_line(line))
+        if (read_line(line) != NULL)
         {
             execute_line(line);
         }
@@ -30,61 +41,46 @@ int main()
 
 char *read_line(char *line)
 {
-    printf(imprimir_prompt());
-    fflush(stdout);
-    if (fgets(line, COMMAND_LINE_SIZE, stdin))
+    imprimir_prompt();
+    if (fgets(line, COMMAND_LINE_SIZE, stdin) != NULL)
     {
         line[COMMAND_LINE_SIZE - 1] = '\0'; // substituyo el carácter final por \0
         return line;
     } // si es NULL
-    if (feof(stdin))
+    if (feof(stdin) )
     { // CTRL+D
         printf("\radiós");
         exit(0);
     }
     return NULL;
 }
-char *imprimir_prompt()
+void imprimir_prompt()
 {
     char cwd[COMMAND_LINE_SIZE];
     getcwd(cwd, COMMAND_LINE_SIZE);
-    return ("%s %s %c ", getenv("USER"), cwd, PROMPT);
+    printf(MAGENTA_T "%s" RESET ":" CYAN_T "~%s " VERDE_T "%c " RESET, getenv("USER"), cwd, PROMPT);
+    fflush(stdout);
 }
 int execute_line(char *line)
 {
-    char **tokens;
+    char *args[ARGS_SIZE];
     // fragmenta line en tokens
-    parse_args(tokens, line);
-    // comprueba si es un comando interno
-    check_internal(tokens);
+    if(parse_args(args, line)) // si hay tokens
+    { 
+        // comprueba si es un comando interno
+        check_internal(args);
+    }
+    return 0;
 }
 int parse_args(char **args, char *line)
 {
-    char *args[ARGS_SIZE];
-    const char delim[4] = "\t\n\r"; // delimitadores
-    int i = 0;                      // índice
+    const char delim[4] = "\t\n\r "; // delimitadores
+    int i = 0;                       // índice
     // primer token
-    args[0] = strtok(line, delim);
-    if (args[0][0] != '#')
-    {
-        if (DEBUGN1)
-        {
-            fprintf(stderr, GRIS_T "[parse_args()→token 0: %s]\n" RESET, args[0]);
-        }
-    }
-    else
-    {
-        args[0] = NULL;
-        if (DEBUGN1)
-        {
-            fprintf(stderr, GRIS_T "[parse_args()→token 0: #inexistente]\n[parse_args()→token 0 corregido: (null)]\n" RESET);
-        }
-    }
+    args[0] = strtok(line, delim); // no puede ser NULL porque eso lo comprueba read_line
     // resto de tokens
     while (args[i] != NULL && i < ARGS_SIZE - 1)
     {
-        i++;
-        args[i] = strtok(NULL, delim);
         if (args[i][0] != '#')
         {
             if (DEBUGN1)
@@ -100,47 +96,50 @@ int parse_args(char **args, char *line)
                 fprintf(stderr, GRIS_T "[parse_args()→token %d: #inexistente]\n[parse_args()→token %d corregido: (null)]\n" RESET, i);
             }
         }
+        i++;
+        args[i] = strtok(NULL, delim);
     }
     args[ARGS_SIZE - 1] = NULL; // el último token siempre ha de ser NULL
     return i;
 }
 int check_internal(char **args)
 {
-    if (strcmp(args[0], "exit"))
+    if (strcmp(args[0], "exit") == 0)
     {
         exit(0);
         return 1;
     }
-    else if (strcmp(args[0], "cd"))
+    else if (strcmp(args[0], "cd") == 0)
     {
         internal_cd(args);
         return 1;
     }
-    else if (strcmp(args[0], "export"))
+    else if (strcmp(args[0], "export") == 0)
     {
         internal_export(args);
         return 1;
     }
-    else if (strcmp(args[0], "source"))
+    else if (strcmp(args[0], "source") == 0)
     {
         internal_source(args);
         return 1;
     }
-    else if (strcmp(args[0], "jobs"))
+    else if (strcmp(args[0], "jobs") == 0)
     {
         internal_jobs(args);
         return 1;
     }
-    else if (strcmp(args[0], "fg"))
+    else if (strcmp(args[0], "fg") == 0)
     {
         internal_fg(args);
         return 1;
     }
-    else if (strcmp(args[0], "bg"))
+    else if (strcmp(args[0], "bg") == 0)
     {
         internal_bg(args);
         return 1;
     }
+    
     return 0;
 }
 int internal_cd(char **args)
