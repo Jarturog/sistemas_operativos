b2600deebc2fa7ff4e10efdf0f04e0e917dc50d7
Jarturog <69714460+Jarturog@users.noreply.github.com>
2022-12-08 10:45:23 +0100

    Update nivel3.c

--- practica2/nivel3.c
+++ practica2/nivel3.c
@@ -84,7 +84,7 @@ char *read_line(char *line)
     } // si es NULL
     if (feof(stdin))
     { // CTRL+D
-        printf("\radiós");
+        printf("\radiós\n");
         exit(0);
     }
     return NULL;
