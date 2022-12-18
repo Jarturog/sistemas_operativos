5531e3e1bfc7175f6a3746f83290af4a00fd57b6
Jarturog <69714460+Jarturog@users.noreply.github.com>
2022-12-18 18:24:57 +0100

    Update nivel3.c

--- Abaurrea_Gonzalez_Prats/nivel3.c
+++ Abaurrea_Gonzalez_Prats/nivel3.c
@@ -298,8 +298,6 @@ int internal_cd(char **args)
         {
             int tipoComa = args[i][0];
             args[i]++; // paso por encima de la comilla
-            printf("\n%s\n%c", args[i], tipoComa);
-            fflush(stdout);
             while (args[i] != NULL && args[i][0] != '\0' && (args[i][strlen(args[i]) - 1] == 1 || args[i][strlen(args[i]) - 1] == 6))
             {
                 strcat(argsToCwd, args[i]);
