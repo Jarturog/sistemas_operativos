e8199d52dd0c4413a3600677c7c5d986112a6de4
Jarturog <69714460+Jarturog@users.noreply.github.com>
2023-01-08 17:55:23 +0100

    Update stack_counters.c

--- practica3/stack_counters.c
+++ practica3/stack_counters.c
@@ -11,53 +11,56 @@ void *worker(void *ptr);
 int main(int argc, char *argv[])
 {
 
-    
     int *data = 0;
     if (argv[1] != NULL)
     {                                   // verifica si la pila existe
         s = (struct my_stack *)argv[1]; // asigna la pila cuyo nombre del fichero se ha pasado por consola
-        if (s->size < 10)
-        { // si la pila tiene menos de 10 elementos o ninguno
+        if (s->size < 10) // si la pila tiene menos de 10 elementos o ninguno
+        { 
             for (int i = s->size; i < 10; i++)
             {
-                malloc(sizeof(data));   // reserva espacio para el entero
-                my_stack_push(s, data); // se agregan los restantes individualmente
+                if (!(data = malloc(sizeof(data)))) // reserva espacio para el entero
+                { 
+                    fprintf(stderr, "Error reservando memoria con malloc()");
+                    return -1; // si ha ocurrido un error para el proceso y lo imprime
+                }
+                data = malloc(sizeof(data));
+                if (my_stack_push(s, data) < 0) // se agregan los restantes individualmente
+                {
+                    fprintf(stderr, "Error metiendo el elemento en la pila");
+                    return -1; // si ha ocurrido un error para el proceso y lo imprime
+                }
             }
         }
         free(my_stack_purge(s)); // libera el espacio reservado con malloc()
     }
     else
     {
-
-        char mensaje[12000];
-        sprintf(mensaje, "No se ha especificado un nombre de fichero: int argc, char *argv[]");
-        write(2, mensaje, strlen(mensaje)); // 2 es el flujo stderr
-
-
+        fprintf(stderr, "No se ha especificado un nombre de fichero: int argc, char *argv[]");
         s = my_stack_init(NUM_THREADS);
     }
+
     pthread_t *hilos[NUM_THREADS];
-      
+
     for (int i = 0; i < NUM_THREADS; i++)
     {
-        
         pthread_create(*hilos[i], NULL, worker, NULL);
     }
     pthread_join(mutex, NULL);
-    my_stack_write(s,argv);
+    my_stack_write(s, argv[1]);
     pthread_exit(0);
 }
+
 void *worker(void *ptr)
 {
     for (int i = 0; i < N; i++)
     {
-        pthread_mutex_lock(mutex) ;
-        int *data;
-        data = (int) my_stack_pop(s);  //extrae un elemento de la pila
-        data++;  //incrementa el valor
+        pthread_mutex_lock(mutex);
+        int *data = (int *)my_stack_pop(s); // extrae un elemento de la pila
+        *data++;                            // incrementa el valor
         pthread_mutex_unlock(mutex);
-        pthread_mutex_lock(mutex) ;
-        my_stack_push(s,data);  //se almacena de nuevo en la pila
+        pthread_mutex_lock(mutex);
+        my_stack_push(s, data); // se almacena de nuevo en la pila
         pthread_mutex_unlock(mutex);
     }
     pthread_exit(NULL);
